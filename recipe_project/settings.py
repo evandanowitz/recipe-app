088781d0f5a42fd3e116947303d0adb6b2978794
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2025-01-23 16:24:40 -0500

    Link recipes app in INSTALLED_APPS

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # recipe application related apps
+    'recipes'
 ]
 
 MIDDLEWARE = [
