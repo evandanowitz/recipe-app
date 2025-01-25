3f44da917f446e66e944343c53df04d8bcf284ff
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2025-01-25 17:57:38 -0500

    Define home view function to render recipes home template

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# This function takes the request coming from the web application and, 
+# returns the template available at recipes/home.html as a response
+def home(request):
+  return render(request, 'recipes/recipes_home.html')
