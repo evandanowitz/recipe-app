304bcc993ed0e89815f6ecb82dc0629fd620ea67
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2025-02-11 12:35:03 -0500

    Added 'pic' attribute to Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -4,14 +4,16 @@ from django.shortcuts import reverse
 # Create your models here.
 
 class Recipe(models.Model):
-  name = models.CharField(max_length=100)
+  name = models.CharField(max_length=120)
   cooking_time = models.IntegerField(help_text='in minutes')
   ingredients = models.TextField(
-    max_length=500,
-    help_text='Enter ingredients as a comma-separated list'
+    max_length = 500,
+    help_text = 'Enter ingredients as a comma-separated list',
+    blank=True # Allows empty submissions in forms
   )
-  difficulty = models.CharField(max_length=25)
+  difficulty = models.CharField(max_length=25, editable=False)
   description = models.TextField()
+  pic = models.ImageField(upload_to='recipes', default='no_picture.jpg')
 
   def __str__(self):
     return str(self.name)
