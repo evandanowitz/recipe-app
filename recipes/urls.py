7d3c2c9040ce1afa99d5d130e44b87f07b9a47cd
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2025-02-11 12:43:31 -0500

    Map views to specific URLs

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, recipe_list, RecipeDetailView
 
 app_name = 'recipes'
 
 urlpatterns = [
-  path('', home),
+  path('', home, name='home'), # the URL to list all recipes
+  path('recipes/', recipe_list, name='recipe_list'),
+  path('recipes/<int:pk>/', RecipeDetailView.as_view(), name='recipe_detail'), # <pk> param indicates the primary key of the object
 ]
