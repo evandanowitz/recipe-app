914f030fc086270e9fbbefa784682211ab6f90f0
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2025-02-11 12:32:07 -0500

    Extend the project-level urlpatterns parameter to include the media information

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
-from django.urls import include
+from django.urls import path, include
+from django.conf import settings # allows you to access the MEDIA_URL and MEDIA_ROOT variables that you need to add.
+from django.conf.urls.static import static # provides access to the Django helper function static( ), which allows you to create URLs from local folder names.
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('recipes.urls'))
+    path('', include('recipes.urls')),
+    # path('recipes/', include('recipes.urls'))
 ]
+
+# extends urlpatterns parameter to include the media info.
+# have now specifies the URL "/media/" that will trigger this media view.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
