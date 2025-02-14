ecddb8037ac49c47a266df442771ec19e718ed5f
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2025-02-13 19:27:29 -0500

    Update class definitions to inherit from the LoginRequiredMixin to protect views

--- recipes/views.py
+++ recipes/views.py
@@ -1,22 +1,25 @@
-from django.shortcuts import render                     # imported by default
-from .models import Recipe                              # to access the Recipe model
-from django.views.generic import ListView, DetailView   # to display lists and details
+from django.shortcuts import render                           # imported by default
+from .models import Recipe                                    # to access the Recipe model
+from django.views.generic import ListView, DetailView         # to display lists and details
+from django.contrib.auth.mixins import LoginRequiredMixin     # to protect class-based view
+from django.contrib.auth.decorators import login_required     # to protect function-based view
 
 # Create your views here.
 
-class RecipeListView(ListView):                   # class-based view
-  model = Recipe                                  # specify model
-  template_name = 'recipes/recipes_home.html'     # specify template
+class RecipeListView(LoginRequiredMixin, ListView):           # class-based "protected" view
+  model = Recipe                                              # specify model
+  template_name = 'recipes/recipes_home.html'                 # specify template
 
-class RecipeDetailView(DetailView):               # class-based view
-  model = Recipe                                  # specify model
-  template_name = 'recipes/recipe_details.html'   # specify template
+class RecipeDetailView(LoginRequiredMixin, DetailView):       # class-based "protected" view
+  model = Recipe                                              # specify model
+  template_name = 'recipes/recipe_details.html'               # specify template
 
 # This function takes the request coming from the web application and, 
 # returns the template available at recipes/home.html as a response
 def home(request):
   return render(request, 'recipes/recipes_home.html')
 
+@login_required # protected
 def recipe_list(request):
   # retrieve all recipes from the database
   recipes = Recipe.objects.all()
