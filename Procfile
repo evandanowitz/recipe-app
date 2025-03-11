8ce50ddde7f9710c0b52cecacff454435ba59d15
evandanowitz <139590465+evandanowitz@users.noreply.github.com>
2025-03-11 18:54:19 -0400

    Fix Python path for Heroku deployment

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 <!-- This file declares the process type and entry point of the application. -->
+text
 
 web: gunicorn recipe_project.wsgi --log-file -
--- recipe_project/wsgi.py
+++ recipe_project/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_project.settings')
+# Add the project root to the Python path
+project_root = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(project_root)
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recipe_project.settings')
 application = get_wsgi_application()
